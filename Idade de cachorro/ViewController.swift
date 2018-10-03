fc3082136809c6d5a58cd1ccd7bc770360d8c72b
gilmar <jiraspiom@yahoo.com.br>
2018-10-03 11:05:13 -0300

    Subindo o projeto ja finalizado

--- Idade de cachorro/ViewController.swift
+++ Idade de cachorro/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //fazendo link do label
     @IBOutlet weak var legendaResultado: UILabel!
+    
+    //fazendo link do texto
     @IBOutlet weak var campoIdadeCachorro: UITextField!
     
+    //link do botão
     @IBAction func descobrirIdade(_ sender: Any) {
-    
+        //criado variavel para receber um texto que o mesmo foi convertido para int
         let idade = Int(campoIdadeCachorro.text!)! * 7
+        
+        //convertendo um inteiro para string e muito pratico, e seu resultado e mostrado
         legendaResultado.text = "A idade de cachorro em anos humanos é:" + String(idade)
         
     }
